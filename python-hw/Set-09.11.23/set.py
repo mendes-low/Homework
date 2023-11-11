f6ac816ddec32ac345bd13dc8176ea820e5a2ed7
mendes-low <uskembaev07@icloud.com>
2023-11-11 19:38:32 +0600

    hw 2

---
+++ python-hw/Set-09.11.23/set.py
@@ -0,0 +1,91 @@
+# Level 1
+
+# 1
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# print(len(it_companies))
+
+# 2
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies.add('Twitter')
+# print(it_companies)
+
+# 3
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies_2 = {'Youtube' , 'WhatsApp' , 'Telegram'}
+# it_companies.update(it_companies_2)
+# print(it_companies)
+
+# 4
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies.remove('IBM')
+# print(it_companies)
+
+# 5
+# Ошибка шықпайды
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies.discard('Telegram')
+# print(it_companies)
+
+# Ошибка шығады
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies.remove('Telegram')
+# print(it_companies)
+
+
+# Level 2
+
+# 1
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.union(b))
+
+# 2
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.intersection(b))
+
+# 3
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.issubset(b))
+
+# 4
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.isdisjoint(b))
+
+# 5
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.union(b))
+# print(b.union(a))
+
+# 6
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# print(a.symmetric_difference(b))
+
+# 7
+# a = {19, 22, 24, 20, 25, 26}
+# b = {19, 22, 20, 25, 26, 24, 28, 27}
+# del a
+# del b
+
+
+# Level 3
+
+# 1
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+
+
+# 2
+
+
+
+
+# 3
+sentence = {'I am a teacher and I love to inspire and teach people'}
+print(sentence)
+
+
