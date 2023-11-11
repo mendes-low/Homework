90141418ba2da0c11744635dc0ba3fc20e5a7935
mendes-low <uskembaev07@icloud.com>
2023-11-11 22:24:00 +0600

    hw 2

--- python-hw/Set-09.11.23/set.py
+++ python-hw/Set-09.11.23/set.py
@@ -11,8 +11,7 @@
 
 # 3
 # it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-# it_companies_2 = {'Youtube' , 'WhatsApp' , 'Telegram'}
-# it_companies.update(it_companies_2)
+# it_companies.update(['Youtube' , 'WhatsApp' , 'Telegram'])
 # print(it_companies)
 
 # 4
@@ -75,17 +74,41 @@
 # Level 3
 
 # 1
-age = [22, 19, 24, 25, 26, 24, 25, 24]
-
-
+# age = [22 , 19 , 24 , 25 , 26 , 24 , 25 , 24]
+# print(len(age))
+# print(len(set(age)))
+# print(len(set(age)) < len(age))
 
 # 2
+# String = '''
+# Strings are an integrable data structure. 
+# There are two ways to iterate over the elements of strings by characters and by indexes. 
+# Each element of the string is represented by one character. 
+# Strings are an immutable data structure.
+# '''
 
+# List = '''
+# Lists are ordered. 
+# The list item can be accessed by index. Lists are mutable types. 
+# The list can store the number of different elements.
+# '''
 
+# Tuble = '''
+# Tuples in Python are the same lists with one exception. 
+# Tuples are immutable data structures. 
+# Just like lists, they can consist of elements of different types, separated by commas.
+# Tuples are enclosed in round brackets, not square brackets.
+# '''
 
+# Set = '''
+# Set is an unordered collection of unique elements. 
+# Unique, that is, there are no duplicate values in it.
+# '''
 
 # 3
-sentence = {'I am a teacher and I love to inspire and teach people'}
-print(sentence)
+# sentence = 'I am a teacher and I love to inspire and teach people'
+# sentence_set = sentence.split()
+# sentence = set(sentence_set)
+# print(sentence)
 
 
