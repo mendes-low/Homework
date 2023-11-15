13ced31c70572c1baf9025fb764faa5f169fb5a2
mendes-low <uskembaev07@icloud.com>
2023-11-15 17:00:01 +0600

    hw 3

---
+++ python-hw/Dictionaries-15.11.23/dct.py
@@ -0,0 +1,57 @@
+# 1
+# dog = dict()
+
+# 2
+# dog = {
+#     'name':'Max',
+#     'color':'white',
+#     'breed':'husky',
+#     'legs':4,
+#     'age':5
+# }
+# print(dog)
+
+# 3
+student = {
+    'first_name':'Aziz',
+    'last_name':'Uskembaev',
+    'gender':'men',
+    'age':16,
+    'marital_status':False,
+    'skills':['HTML' , 'CSS' , 'JS'], 
+    'country':'Kazakhstan',
+    'city':'Almaty',
+    'address':'Almaty Arena'
+}
+# print(student)
+
+# 4
+# print(len(student))
+
+# 5
+# print(student['skills'])
+
+# 6
+# student['skills'].append('Ruby')
+# print(student)
+
+# 7
+# student_keys = student.keys()
+# print(student_keys)
+
+# 8
+# student_value = student.values()
+# print(student_value)
+
+# 9
+# student_lists = student.items()
+# print(student_lists)
+
+# 10
+print(student.pop('address'))
+
+
+
+
+
+
