25f05bb16bcf70169f22d6f8ac228b82c13bdb03
mendes-low <uskembaev07@icloud.com>
2023-11-26 20:52:01 +0600

    hw 5

--- python-hw/loops-23.11.23/loops.py
+++ python-hw/loops-23.11.23/loops.py
@@ -23,13 +23,271 @@
 # 3
 # (1 variant)
 # for i in range(1,8):
-#     print('#'*i)
+#     print('#' * i)
 
 # (2 variant)
-count = 1
-while count < 10:
-    print()
+# count = 1
+# while count <= 10:
+#     print('#' * count)
+#     count = count + 1
+
+4
+# (1 variant)
+# for i in range(1,8):
+#     for x in range():
+#         print()
 
 
 
-# 4
+# 5
+# (1 variant)
+# for i in range(0,11):
+#     print(f'{i} x {i} = {i*i}')
+
+# (2 variant)
+# count = 0
+# while count <= 10:
+#     print(f'{count} x {count} = {count*count}')
+#     count = count + 1
+
+# 6
+# languages = ['Python', 'Numpy','Pandas','Django', 'Flask'] 
+# for i in languages:
+#     print(i)
+
+# 7
+# for i in range(1,101):
+#     if i % 2 == 0:
+#         print(i)
+
+# 8
+# for i in range(1,101):
+#     if i % 2 == 1:
+#         print(i)
+
+# Level 2
+
+1
+# for i in range(1,101):
+    
+
+
+
+
+
+
+
+
+
+# Level 3
+
+# 1
+# countries = [
+#   'Afghanistan',
+#   'Albania',
+#   'Algeria',
+#   'Andorra',
+#   'Angola',
+#   'Antigua and Barbuda',
+#   'Argentina',
+#   'Armenia',
+#   'Australia',
+#   'Austria',
+#   'Azerbaijan',
+#   'Bahamas',
+#   'Bahrain',
+#   'Bangladesh',
+#   'Barbados',
+#   'Belarus',
+#   'Belgium',
+#   'Belize',
+#   'Benin',
+#   'Bhutan',
+#   'Bolivia',
+#   'Bosnia and Herzegovina',
+#   'Botswana',
+#   'Brazil',
+#   'Brunei',
+#   'Bulgaria',
+#   'Burkina Faso',
+#   'Burundi',
+#   'Cambodia',
+#   'Cameroon',
+#   'Canada',
+#   'Cape Verde',
+#   'Central African Republic',
+#   'Chad',
+#   'Chile',
+#   'China',
+#   'Colombi',
+#   'Comoros',
+#   'Congo (Brazzaville)',
+#   'Congo',
+#   'Costa Rica',
+#   "Cote d'Ivoire",
+#   'Croatia',
+#   'Cuba',
+#   'Cyprus',
+#   'Czech Republic',
+#   'Denmark',
+#   'Djibouti',
+#   'Dominica',
+#   'Dominican Republic',
+#   'East Timor (Timor Timur)',
+#   'Ecuador',
+#   'Egypt',
+#   'El Salvador',
+#   'Equatorial Guinea',
+#   'Eritrea',
+#   'Estonia',
+#   'Ethiopia',
+#   'Fiji',
+#   'Finland',
+#   'France',
+#   'Gabon',
+#   'Gambia, The',
+#   'Georgia',
+#   'Germany',
+#   'Ghana',
+#   'Greece',
+#   'Grenada',
+#   'Guatemala',
+#   'Guinea',
+#   'Guinea-Bissau',
+#   'Guyana',
+#   'Haiti',
+#   'Honduras',
+#   'Hungary',
+#   'Iceland',
+#   'India',
+#   'Indonesia',
+#   'Iran',
+#   'Iraq',
+#   'Ireland',
+#   'Israel',
+#   'Italy',
+#   'Jamaica',
+#   'Japan',
+#   'Jordan',
+#   'Kazakhstan',
+#   'Kenya',
+#   'Kiribati',
+#   'Korea, North',
+#   'Korea, South',
+#   'Kuwait',
+#   'Kyrgyzstan',
+#   'Laos',
+#   'Latvia',
+#   'Lebanon',
+#   'Lesotho',
+#   'Liberia',
+#   'Libya',
+#   'Liechtenstein',
+#   'Lithuania',
+#   'Luxembourg',
+#   'Macedonia',
+#   'Madagascar',
+#   'Malawi',
+#   'Malaysia',
+#   'Maldives',
+#   'Mali',
+#   'Malta',
+#   'Marshall Islands',
+#   'Mauritania',
+#   'Mauritius',
+#   'Mexico',
+#   'Micronesia',
+#   'Moldova',
+#   'Monaco',
+#   'Mongolia',
+#   'Morocco',
+#   'Mozambique',
+#   'Myanmar',
+#   'Namibia',
+#   'Nauru',
+#   'Nepal',
+#   'Netherlands',
+#   'New Zealand',
+#   'Nicaragua',
+#   'Niger',
+#   'Nigeria',
+#   'Norway',
+#   'Oman',
+#   'Pakistan',
+#   'Palau',
+#   'Panama',
+#   'Papua New Guinea',
+#   'Paraguay',
+#   'Peru',
+#   'Philippines',
+#   'Poland',
+#   'Portugal',
+#   'Qatar',
+#   'Romania',
+#   'Russia',
+#   'Rwanda',
+#   'Saint Kitts and Nevis',
+#   'Saint Lucia',
+#   'Saint Vincent',
+#   'Samoa',
+#   'San Marino',
+#   'Sao Tome and Principe',
+#   'Saudi Arabia',
+#   'Senegal',
+#   'Serbia and Montenegro',
+#   'Seychelles',
+#   'Sierra Leone',
+#   'Singapore',
+#   'Slovakia',
+#   'Slovenia',
+#   'Solomon Islands',
+#   'Somalia',
+#   'South Africa',
+#   'Spain',
+#   'Sri Lanka',
+#   'Sudan',
+#   'Suriname',
+#   'Swaziland',
+#   'Sweden',
+#   'Switzerland',
+#   'Syria',
+#   'Taiwan',
+#   'Tajikistan',
+#   'Tanzania',
+#   'Thailand',
+#   'Togo',
+#   'Tonga',
+#   'Trinidad and Tobago',
+#   'Tunisia',
+#   'Turkey',
+#   'Turkmenistan',
+#   'Tuvalu',
+#   'Uganda',
+#   'Ukraine',
+#   'United Arab Emirates',
+#   'United Kingdom',
+#   'United States',
+#   'Uruguay',
+#   'Uzbekistan',
+#   'Vanuatu',
+#   'Vatican City',
+#   'Venezuela',
+#   'Vietnam',
+#   'Yemen', 
+#   'Zambia', 
+#   'Zimbabwe'
+# ]
+# for i in countries:
+#     if 'land' in i:
+#         print(i)
+
+
+2
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# reversed = ''
+# for i in fruits:
+#     print([i])
+#     reversed = i + reversed
+# print(reversed)
+
+3
